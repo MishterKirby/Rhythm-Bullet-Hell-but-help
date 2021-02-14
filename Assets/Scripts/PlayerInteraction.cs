e3c5f58db49839774993536821c15e79878713ff
Sebastian Rivera <ilovepikachu170@gmail.com>
2021-02-13 21:49:55 -0600

    Commit initial scripts

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour {
-
+public float Horizontal;
+public float Vertical;
+public Rigidbody2D rb2d;
+public float e;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		Horizontal = Input.GetAxis("Horizontal");
+		Vertical = Input.GetAxis("Vertical");
+
+		rb2d.AddForce(new Vector2(Horizontal*e, Vertical*e));
 	}
 }
